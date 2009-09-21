f3080bbf705ff66ae99a72109ee5f6214626c458
clackn <clackn@fbdb9df1-db47-0410-955a-fa45b24b025c>
2009-09-21 17:37:41 +0000

    added code to open, close and write bars
    git-svn-id: https://subversion.int.janelia.org/myerslab/projects/whisker/branches/2009-05-25@443 fbdb9df1-db47-0410-955a-fa45b24b025c

--- bar_io.c
+++ bar_io.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 
 //
 // Bar type
@@ -26,15 +27,20 @@ typedef BarFile FILE;
 //
 //
 BarFile* Bar_File_Open(const char* filename, const char *mode)
-{
+{ BarFile* f = fopen(filename, mode);
+  if(!f)
+    error("Could not open bar file\n"
+          "\tat %s\n"
+          "\twith mode %s\n" , filename, mode);
+  return f;
 }
 
 void Bar_File_Close( BarFile *file )
-{
+{ if(file) fclose(file);
 }
 
 void Bar_File_Append_Bar( BarFile *file,  Bar* b )
-{
+{ fprintf(file,"%d %g %g\n",b->time,b->x,b->y);
 }
 
 Bar *Read_Bars( BarFile *file, int *n )
