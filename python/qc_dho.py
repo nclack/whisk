55fe10e6e7fd74ccee2e0678ac875bdc765ac411
Nathan Clack <clackn@janelia.hhmi.org>
2011-10-26 17:17:21 -0400

    Work on analyzing DHO's data

--- python/qc_dho.py
+++ python/qc_dho.py
@@ -1,5 +1,6 @@
 """
-  >>> meta = meta()
+  >>> meta = loadmeta()
+  >>> m2 = filter_trial_type(meta,'CR')
 """
 import traj
 from numpy import *
@@ -95,8 +96,17 @@ def aspath(anm,date,root,tid,ext):
     anm,
     anm+'x'+dstr,
     anm+'x'+dstr+('_%04d'%tid)+ext])
+def aspattern(anm,date,root,tid,ext):
+  import os
+  dstr = date.strftime("%m%d%y")
+  return reduce(os.path.join,[
+    root,
+    anm,
+    anm+'x'+dstr,
+    ('*_%04d'%tid)+ext])
 
-def gen_trials_w_keys(meta,anm=None,date=None):
+
+def gen_trials_w_keys(meta,anm=None,date=None,must_exist=True):
   import os
   if anm is None:
     for anm in meta.iterkeys():
@@ -112,7 +122,7 @@ def gen_trials_w_keys(meta,anm=None,date=None):
       date = asdate(date) #<date> can be passed in as a string or datetime object
       for itrial,tdata in enumerate(meta.get(anm,{}).get(date,{}).get('trials',[])):
         fname = aspath(anm,date,ROOT,tdata['id'],'.measurements')
-        if os.path.exists(fname):
+        if (not must_exist) or os.path.exists(fname):
           yield (anm,date,itrial),tdata
 
 def gen_measurements_w_keys(meta,anm=None,date=None):
@@ -123,6 +133,34 @@ def gen_measurements_w_keys(meta,anm=None,date=None):
 def gen_measurements(meta,anm=None,date=None): 
   for (anm,date,itrial),fname in gen_measurements_w_keys(meta,anm,date):
     yield fname
+
+def fix_filenames(meta,anm=None,date=None,fakeit=True):
+  """ 
+  This function exists because DHO had accidently mis-named a few files.
+  Keeping it here just for future reference.
+  """
+  import glob,shutil
+  for (a,d,i),v in gen_trials_w_keys(meta,anm,date,must_exist=False):
+    fname = aspath(a,d,ROOT,v['id'],'.measurements')
+    if not os.path.exists(fname):
+      pattern = aspattern(a,d,ROOT,v['id'],'.measurements')
+      files = glob.glob(pattern)
+      if len(files)!=1: # handle them errors, bro
+        if len(files)==0:
+          print "Could not find candidate for missing file: "+fname
+        else:
+          print "Multiple candidates found for missing file: "+fname
+          for f in files:
+            print "\t"+f
+        continue
+      # done handling errors, get some buisness done
+      print "Move %s to %s"%(files[0],fname)
+      if not fakeit:
+        shutil.move(files[0],fname)
+
+
+
+
 
 def head(g):
   return g.next()
@@ -172,7 +210,7 @@ def awrap(th):
   v[th>0.0] = -180.0;
   return th+v
 
-def build_histograms(fname,min_ms,max_ms,norm=True):
+def build_histograms(fname,min_ms,max_ms,norm=True,dth_deg=2.5):
   data = traj.MeasurementsTable(fname).asarray()  
   out = {}
 
@@ -185,7 +223,7 @@ def build_histograms(fname,min_ms,max_ms,norm=True):
   
   # angle of most posterior whisker
   mask = wmask*tmask
-  nn,xx = histogram(awrap(data[mask,5]),arange(-180.0,180.0,1.0),density=norm)
+  nn,xx = histogram(awrap(data[mask,5]),arange(-180.0,180.0,dth_deg),density=norm)
   out['angle'] = (xx[1:],nn)
   
   # avg angle change between whiskers in the same frame
@@ -196,32 +234,71 @@ def build_histograms(fname,min_ms,max_ms,norm=True):
     mask = (data[:,0]==i)*tmask
     angles[data[mask,1].astype(int),i] = awrap(data[mask,5])
   dth = diff(angles,axis=1).mean(axis=1)
-  nn,xx = histogram(dth,arange(-180.0,180.0,1.0),density=norm)
+  nn,xx = histogram(dth,arange(-180.0,180.0,dth_deg),density=norm)
   out['spread'] = (xx[1:],nn)
 
   #all angles
   mask = tmask*(data[:,0]>=0)*(data[:,0]<=2) # actually only accept most posterior 3
-  nn,xx = histogram(awrap(data[mask,5]),arange(-180.0,180.0,1.0))
+  nn,xx = histogram(awrap(data[mask,5]),arange(-180.0,180.0,dth_deg))
   out['all'] = (xx[1:],nn)
   return out
 
-def hist_matrix(meta,anm,date,min_ms=0,max_ms=2000,key='angle',norm=True):
+def hist_matrix(meta,anm,date,min_ms=0,max_ms=2000,key='angle',norm=True,dth_deg=2.5):
   ntrials = len(list(gen_trials_w_keys(meta,anm,date)))
-  t=build_histograms(head(gen_measurements(meta)),min_ms,max_ms,norm=norm)
+  t=build_histograms(head(gen_measurements(meta,anm,date)),min_ms,max_ms,norm=norm,dth_deg=dth_deg)
   nbins = t[key][0].shape[0]
 
   m = nan*zeros((ntrials,nbins))
   for i,fname in enumerate(gen_measurements(meta,anm,date)):
     try:
-      m[i,:] = build_histograms(fname,min_ms,max_ms,norm=norm)[key][1]
+      m[i,:] = build_histograms(fname,min_ms,max_ms,norm=norm,dth_deg=dth_deg)[key][1]
     except UnknownFrameRate:
       pass
   return m[~isnan(m).all(axis=1),:] #filter out trials with unknown frame rate
 
-def plot_hist_matrix_means(meta,anm,**kwargs):
-  days = sorted([d for d in map(str,meta[anm]) if type(d) is datetime.date])
-  for d in days:
-      out = hist_matrix(meta,anm,d,**kwargs)
-      plot(out)
-  legend(days)
+def plot_hist_matrix_means(meta,**kwargs):
+  for anm in meta.keys():
+    try:
+      figure()
+      days = sorted([str(d) for d in meta[anm.upper()] if type(d) is datetime.date])
+      for d in days:
+          out = hist_matrix(meta,anm,d,**kwargs)
+          plot(out.mean(axis=0))
+      legend(days)
+      title(anm)
+    except:
+      pass
+
+def plot_hist_matrix_over_time(meta,anm,day,from_ms,to_ms,every_ms,**kwargs):
+  ncols = len(range(from_ms,to_ms,every_ms))
+  kwargs['min_ms'] = from_ms
+  kwargs['max_ms'] = from_ms+every_ms
+  tmp = hist_matrix(meta,meta.keys()[0],None,**kwargs)
+  nrows = tmp.shape[1]
+  out = nan*zeros((nrows,ncols)) #cols are time, rows are feature, value is density
+
+  figure()
+  for i,t in enumerate(xrange(from_ms,to_ms,every_ms)):
+    kwargs['min_ms'] = t
+    kwargs['max_ms'] = t+every_ms
+    tmp = hist_matrix(meta,anm,day,**kwargs)
+    out[:,i] = tmp.mean(axis=0)
+  imshow(log(out+0.001),interpolation='nearest')
+  xlabel('time')
+  title(anm + ':' + str(day))
+  axis('tight')
+  colorbar()
+  show()
+  return out
+
+def rates(meta):
+  out = {}
+  for anm in meta.iterkeys():
+    days = sorted([d for d in meta[anm].keys() if type(d) is datetime.date])
+    counts = zeros((len(days),4))  #there are four types: (1)Hit, (2)Miss, (3)FA, (4)CR
+    for iday,day in enumerate(days):
+      for t in meta[anm][day]['trials']:
+        counts[iday,t['typeid']-1] += 1
+    out[anm] = counts
+  return out
 
